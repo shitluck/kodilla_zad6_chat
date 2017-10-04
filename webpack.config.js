7d35373e90d92e44fe1928e8ba7b0037bd7b5860
Mac <fuji.two6@gmail.com>
2017-10-04 11:15:51 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -25,11 +25,11 @@ if (env === 'production') {
 }
 
 module.exports = {
-    entry: (env !== 'production' : [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-    ] : []).concat(['./client/index.js']),
+    entry: (env !== 'production' ? [
+            'react-hot-loader/patch',
+            'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/only-dev-server',
+        ] : []).concat(['./client/index.js']),
     output: {
         path: path.resolve(__dirname, './public'),
         filename: './bundle.js'
